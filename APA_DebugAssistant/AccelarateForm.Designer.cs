12fdbe605dc65b3c597c9a222e9facc0a8267e4d
朱国华 <zgh_email@163.com>
2019-07-15 14:12:09 +0800

    添加加速度显示

---
+++ APA_DebugAssistant/AccelarateForm.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace APA_DebugAssistant
+{
+    partial class AccelarateForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.acc_chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.acc_chart)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.acc_chart, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(898, 484);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // acc_chart
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.acc_chart.ChartAreas.Add(chartArea3);
+            this.acc_chart.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend3.Name = "Legend1";
+            this.acc_chart.Legends.Add(legend3);
+            this.acc_chart.Location = new System.Drawing.Point(3, 3);
+            this.acc_chart.Name = "acc_chart";
+            this.acc_chart.Size = new System.Drawing.Size(892, 438);
+            this.acc_chart.TabIndex = 0;
+            this.acc_chart.Text = "chart1";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 180F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 447);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(892, 34);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "图象清除";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // AccelarateForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(898, 484);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "AccelarateForm";
+            this.Text = "AccelarateForm";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.acc_chart)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart acc_chart;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ APA_DebugAssistant/AccelarateForm.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace APA_DebugAssistant
+{
+    public partial class AccelarateForm : Form
+    {
+        Series VehicleAccelerateWaveTarget = new Series();
+        Series VehicleAccelerateWaveActual = new Series();
+        Series VehicleAccelerateWaveLon = new Series();
+
+        public AccelarateForm()
+        {
+            InitializeComponent();
+
+            acc_chart.Series.Add(VehicleAccelerateWaveTarget);
+            acc_chart.Series.Add(VehicleAccelerateWaveActual);
+            acc_chart.Series.Add(VehicleAccelerateWaveLon);
+
+            acc_chart.ChartAreas[0].AxisY.Maximum = 1;
+            acc_chart.ChartAreas[0].AxisY.Minimum = -1;
+            acc_chart.ChartAreas[0].AxisY.Interval = 0.1;
+
+            VehicleAccelerateWaveTarget.ChartType = SeriesChartType.FastLine;
+            VehicleAccelerateWaveTarget.BorderWidth = 5;
+            VehicleAccelerateWaveTarget.BorderDashStyle = ChartDashStyle.Dash;
+            VehicleAccelerateWaveTarget.Color = Color.Green;
+            VehicleAccelerateWaveTarget.IsVisibleInLegend = true;
+            VehicleAccelerateWaveTarget.LegendText = "目标加速度";
+
+            VehicleAccelerateWaveActual.ChartType = SeriesChartType.FastLine;
+            VehicleAccelerateWaveActual.BorderWidth = 3;
+            VehicleAccelerateWaveActual.BorderDashStyle = ChartDashStyle.Solid;
+            VehicleAccelerateWaveActual.Color = Color.Red;
+            VehicleAccelerateWaveActual.IsVisibleInLegend = true;
+            VehicleAccelerateWaveActual.LegendText = "实际控制加速度";
+
+            VehicleAccelerateWaveLon.ChartType = SeriesChartType.FastLine;
+            VehicleAccelerateWaveLon.BorderWidth = 3;
+            VehicleAccelerateWaveLon.BorderDashStyle = ChartDashStyle.Solid;
+            VehicleAccelerateWaveLon.Color = Color.Blue;
+            VehicleAccelerateWaveLon.IsVisibleInLegend = true;
+            VehicleAccelerateWaveLon.LegendText = "实际反馈加速度";
+
+        }
+
+        /// <summary>
+        /// 清除波形数据
+        /// </summary>
+        private void WaveformClear()
+        {
+            VehicleAccelerateWaveTarget.Points.Clear();
+            VehicleAccelerateWaveActual.Points.Clear();
+            VehicleAccelerateWaveLon.Points.Clear();
+        }
+
+        public void VehicleAcceleratePointAdd(double targetV,double controlV, double actualV)
+        {
+            VehicleAccelerateWaveTarget.Points.AddY(targetV);
+            VehicleAccelerateWaveActual.Points.AddY(controlV);
+            VehicleAccelerateWaveLon.Points.AddY(actualV);
+
+            while (VehicleAccelerateWaveTarget.Points.Count > 50)
+            {
+                VehicleAccelerateWaveTarget.Points.RemoveAt(0);
+            }
+            while (VehicleAccelerateWaveActual.Points.Count > 50)
+            {
+                VehicleAccelerateWaveActual.Points.RemoveAt(0);
+            }
+            while (VehicleAccelerateWaveLon.Points.Count > 50)
+            {
+                VehicleAccelerateWaveLon.Points.RemoveAt(0);
+            }
+        }
+
+        #region 事件
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WaveformClear();
+        }
+        #endregion
+
+    }
+}
